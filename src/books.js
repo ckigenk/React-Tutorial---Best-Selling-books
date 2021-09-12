4ab13f8edd2a14151cfe1ee23cc5a20c8f4b848f
ckigen.k <ckigen.k@gmail.com>
2021-09-12 14:19:15 +0300

    initial commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71tqt4VL%2BdS._AC_UL200_SR200,200_.jpg",
+    title: "It ends with us",
+    author: "Colleen Hoover",
+  },
+
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81icCFFuGUS._AC_UL200_SR200,200_.jpg",
+    title: "American  Marxism",
+    author: "Mark R. Levin",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL200_SR200,200_.jpg",
+    title: "Countdown bin Laden: The Untold Story of…",
+    author: "Chris Wallace",
+  },
+];
